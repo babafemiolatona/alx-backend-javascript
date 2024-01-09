e1380514eca307bb166bcd9b316318c92ff35099
babafemiolatona <femiolatona@gmail.com>
2024-01-10 00:14:29 +0100

    Typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,10 +2,9 @@ const createInt8TypedArray = (length, position, value) => {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
-  return buffer;
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
 };
 
 module.exports = createInt8TypedArray;
