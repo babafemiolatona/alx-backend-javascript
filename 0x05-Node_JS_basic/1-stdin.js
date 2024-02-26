34e698c401d64fb58708d8b9a6d69505cea7b250
babafemiolatona <femiolatona@gmail.com>
2024-02-26 21:36:55 +0100

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,15 @@
-const readline = require('readline');
-
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-r1.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  r1.close();
-});
-
-r1.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
