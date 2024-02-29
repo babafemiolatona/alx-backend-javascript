40471d45e50716daa38eef0cf3f4eba83d931ea2
babafemiolatona <femiolatona@gmail.com>
2024-02-29 17:09:35 +0100

    Refactored code

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,19 +1,15 @@
 const calculateNumber = (type, a, b) => {
-  if (type === 'SUM') {
-    a = Math.round(a);
-    b = Math.round(b);
-    return (a + b);
-  } else if (type === 'SUBTRACT') {
-    a = Math.round(a);
-    b = Math.round(b);
-    return (a - b);
-  } else if (type === 'DIVIDE') {
-    if (b === 0) {
-      return 'Error';
-    }
-    a = Math.round(a);
-    b = Math.round(b);
-    return (a / b);
+  a = Math.round(a);
+  b = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return a + b;
+    case 'SUBTRACT':
+      return a - b;
+    case 'DIVIDE':
+      if (b === 0) return 'Error';
+      return a / b;
   }
 };
 
