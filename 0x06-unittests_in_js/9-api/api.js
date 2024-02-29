df9735d7f7097848ecf698465ee91ca42bd13e10
babafemiolatona <femiolatona@gmail.com>
2024-02-29 22:03:25 +0100

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const id = req.params.id;
+  res.send({ message: `Payment methods for cart ${id}` });
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,26 @@
+// api.test.js
+
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('./api');
+
+describe('GET /cart/:id', () => {
+  it('should return correct message when id is a number', (done) => {
+    request(app)
+      .get('/cart/1')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('Payment methods for cart 1');
+        done();
+      });
+  });
+
+  it('should return 404 status code when id is not a number', (done) => {
+    request(app)
+      .get('/cart/abc')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+  });
+});
